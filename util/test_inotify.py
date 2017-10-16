98106639c09fb03e445e7c485dc5b7a4ab1c0666
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2017-10-16 16:45:20 -0400

    latest

--- util/test_inotify.py
+++ util/test_inotify.py
@@ -6,6 +6,9 @@ _DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 _LOGGER = logging.getLogger(__name__)
 
+
+watch_directory = '/tmp/test-inotify'
+
 def _configure_logging():
     _LOGGER.setLevel(logging.DEBUG)
 
@@ -24,15 +27,23 @@ def _main():
     try:
         for event in i.event_gen():
             if event is not None:
+
                 (header, type_names, watch_path, filename) = event
-                _LOGGER.info("WD=(%d) MASK=(%d) COOKIE=(%d) LEN=(%d) MASK->NAMES=%s "
-                             "WATCH-PATH=[%s] FILENAME=[%s]",
-                             header.wd, header.mask, header.cookie, header.len, type_names,
-                             watch_path.decode('utf-8'), filename.decode('utf-8'))
+                
                 file_path = os.path.join(watch_path.decode('utf-8'), filename.decode('utf-8'))
-                print("file path : %s" % file_path)
-    finally:
-        i.remove_watch(b'/tmp')
+                
+                if os.path.isfile(file_path):
+
+                    if 'IN_CLOSE_WRITE' in type_names:
+
+                        _LOGGER.info("WD=(%d) MASK=(%d) COOKIE=(%d) LEN=(%d) MASK->NAMES=%s "
+                                 "WATCH-PATH=[%s] FILENAME=[%s]",
+                                 header.wd, header.mask, header.cookie, header.len, type_names,
+                                 watch_path.decode('utf-8'), filename.decode('utf-8'))
+
+                        print("file was written to '%s'" % file_path)
+    # finally:
+    #     i.remove_watch(b'/tmp/test-inotify')
 
 if __name__ == '__main__':
     _configure_logging()
